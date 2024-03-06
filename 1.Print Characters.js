4bcd427c6a5cbfe47304e612e4961917508bd744
Miroslav Tzarev <mtzarev@gmail.com>
2024-03-06 18:35:21 +0200

    Add files via upload

---
+++ 1.Print Characters.js
@@ -0,0 +1,9 @@
+function printCharacters(input) {
+    for (const letter of input) {
+        console.log(letter);
+
+    }
+    console.log();
+}
+printCharacters('AWord');
+printCharacters('Sentence');
